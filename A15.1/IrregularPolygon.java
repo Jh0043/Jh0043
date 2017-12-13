9c84490542d56123475c1c2056aad8deae38851e
Jh0043 <jh0043@pleasantonusd.net>
2017-12-12 19:12:15 -0800

    Edit/Add Driver 12/12/17
    problems with area and perimeter methods.

--- A15.1/IrregularPolygon.java
+++ A15.1/IrregularPolygon.java
@@ -12,9 +12,12 @@ public class IrregularPolygon
     private ArrayList <Point2D.Double> myPolygon;
     
     public IrregularPolygon(){
-       
+        this.myPolygon = new ArrayList<Point2D.Double>();
     }
-    public void add(Point2D.Double aPoint){}
+    public void add(Point2D.Double aPoint){
+        this.myPolygon.add(aPoint);
+    
+        }
     public void draw() {}
     public double perimeter() {
         // -----------UNTESTED------------------
@@ -22,7 +25,7 @@ public class IrregularPolygon
         
         //attempted to code distance formula
         
-        for(int i = 1; i < myPolygon.size(); i++){
+        for(int i = 1; i < myPolygon.size()-1; i++){
             peri = Math.sqrt(Math.pow((myPolygon.get(i+1)).getX() 
                 - (myPolygon.get(i)).getX(), 2)
                 + Math.pow((myPolygon.get(i+1)).getY() 
@@ -38,7 +41,14 @@ public class IrregularPolygon
         //first part starting with x
         double sPy = 0.0;
         //second part starting with y
-        
+        double xOf
+        for(int i = 0; i < myPolygon.size(); i++){
+            myPolygon.get(i).getX();
+                
+        }
+        for(int j = 1; j < myPolygon.size(); j++){
+            myPolygon.get(j).getY();
+        }
         //attempted to code shoelace formula
 
         for(int i = 0; i < myPolygon.size(); i++){
@@ -46,10 +56,16 @@ public class IrregularPolygon
             //    + myPolygon.get(i+1).getX() * myPolygon.get(i+2).getY(); 
             // -1 * (myPolygon.get(i).getY() * myPolygon.get(i+1).getY()
             //    + myPolygon.get(i+1).getY() * myPolygon.get(i+2).getY();
+            
+            //fPx = (myPolygon.get(i)).getX() * (myPolygon.get(i+1)).getY()
+            //        + (myPolygon.get(i+1)).getX() * (myPolygon.get(i+2)).getY(); 
+            //sPy = -1 * ((myPolygon.get(i)).getY() * (myPolygon.get(i+1)).getY()
+            //        + (myPolygon.get(i+1)).getY() * (myPolygon.get(i+2)).getY());
             fPx = (myPolygon.get(i)).getX() * (myPolygon.get(i+1)).getY()
                     + (myPolygon.get(i+1)).getX() * (myPolygon.get(i+2)).getY(); 
             sPy = -1 * ((myPolygon.get(i)).getY() * (myPolygon.get(i+1)).getY()
                     + (myPolygon.get(i+1)).getY() * (myPolygon.get(i+2)).getY());
+            
         }
         //(myPolygon.get(myPolygon.size() - 1)).getX() 
         //    * (myPolygon.get(0)).getY();
@@ -58,5 +74,8 @@ public class IrregularPolygon
         Area = Math.abs((0.5) * (fPx + sPy));
         
         return Area;
+    }
+    public String getName(){
+        return "Jasper Hendra";
     }
 }
